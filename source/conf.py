184bdd38ad4425969a495bbbe0b0faead1c37c2e
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-08-31 21:26:06 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -40,8 +40,8 @@ pygments_style = 'default'
 
 html_static_path = ['assets']
 html_theme = 'sphinx_rtd_theme'
-html_favicon = '../GCC.png'
-html_logo = '../GCC.png'
+html_favicon = 'GCC.png'
+html_logo = 'GCC.png'
 html_theme_options = {
     'logo_only': False,
     'style_nav_header_background': '#343131',
