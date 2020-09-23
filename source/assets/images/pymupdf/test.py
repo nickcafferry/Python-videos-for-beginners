f7328f926fa9623e117d0397cb561654baa37276
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-09-23 17:00:22 +0800

    Update test.py

--- source/assets/images/pymupdf/test.py
+++ source/assets/images/pymupdf/test.py
@@ -8,9 +8,9 @@ doc = fitz.open('demo.pdf')
 
 for pg in range(doc.pageCount):
     page = doc[pg]
-    zoom = int(100)
+    zoom = int(1000)
     rotate = int(0)
-    trans = fitz.Matrix(zoom / 100.0, zoom / 100.0).preRotate(rotate)
+    trans = fitz.Matrix(zoom / 1000.0, zoom / 1000.0).preRotate(rotate)
 
     # create raster image of page (non-transparent)
     pm = page.getPixmap(matrix=trans, alpha=False)
@@ -18,5 +18,5 @@ for pg in range(doc.pageCount):
     # write a PNG image of the page
     # pm.writePNG('%s.png' % pg)
     # pm.writeImage("%s.gif" % pg)
-    img = Image.frombytes("RGB", [1499, 876], pm.samples)
+    img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
     img.save("%s.jpg" % pg, "JPEG")
